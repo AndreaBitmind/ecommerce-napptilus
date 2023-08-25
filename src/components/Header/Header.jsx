bfe8266d0573d0da032eaa6dea5f03e51a372501
Andrea Martinez <andrea.maort@gmail.com>
2023-08-25 17:08:20 +0200

    feat: item counter implemented in header and a button to clear cart

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,16 @@
+import { useCart } from "../../Hooks/useCart";
 import { CartIcon } from "../Icons";
 import "./Header.css";
 
 export function Header() {
+  const { cartItemCount, clearCart } = useCart();
   return (
     <header className="header">
-      <h1>eCommerce Napptilus</h1>
+      <h1>Mobile eCommerce</h1>
       <div className="cart-display">
         <CartIcon />
-        <div>Num</div>
+        <div>{cartItemCount}</div>
+        <button onClick={clearCart}>X</button>
       </div>
     </header>
   );
